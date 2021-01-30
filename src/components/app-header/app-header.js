d01376a16a74b1ed12fd7729892704715a8cdcfd
unknown <olena.denysova88@gmail.com>
2021-01-30 18:02:38 +0100

    mobile menu

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -2,12 +2,14 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
 import cartIcon from './shopping-cart-solid.svg'
+import homeIcon from './home.svg'
 import logo from './logo.png'
 
 import './app-header.scss'
 
 const AppHeader = ({totalPrice}) => {
     return (
+        <>
         <header className="header">
             <Link to = {'/'} className="header__link first" title="home page">
                 <img className="header__logo" src={logo} alt="logo"></img>
@@ -21,6 +23,19 @@ const AppHeader = ({totalPrice}) => {
             <Link to = {'/orders'} className="header__link">Orders</Link>
             <Link to = {'/contact'} className="header__link">Contact us	&#128386;</Link>
         </header>
+
+        <nav className="header__mobile">
+            <Link to = {'/'} className="header__link"><img className="header__cart" src={homeIcon} alt="home"></img></Link>
+            <Link to = {'/menu'} className="header__link">Menu</Link>
+            <Link to = "/cart" className="header__link">
+                <img className="header__cart" src={cartIcon} alt="cart"></img>
+                {totalPrice}$
+            </Link>
+            <Link to = {'/orders'} className="header__link">Order</Link>
+            <Link to = {'/contact'} className="header__link">&#128386;</Link>
+        </nav>
+
+        </>
     )
 }
 
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -9,6 +9,7 @@ class Footer extends Component {
         return (
            <div className="footer">
                <p className="footer__text">React-Restaurant-App made by Elena Riemer &copy;</p>
+               <p className="footer__mobile">Elena Riemer &copy;</p>
                <p className="footer__year">2021</p>
                <a href="mailto:elena.riemer88@gmail.com" className="footer__mail"><p>&#128386; elena.riemer88@gmail.com</p></a>
            </div>
